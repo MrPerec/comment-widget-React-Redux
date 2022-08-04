1f0b492c1817e1c13caa995c21f90c52af5f75a4
MrPerec <MrPerec@mail.ru>
2022-08-04 15:30:16 +0400

    del not used variable, del license

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const babelPluginProposalClassProperties = require('@babel/plugin-proposal-class-properties');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'build.js'
+    filename: 'build.js',
   },
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserJSPlugin({ extractComments: false }), new OptimizeCSSAssetsPlugin({})],
   },
-  plugins: [
-    new HtmlWebpackPlugin({template: './index.html'}),
-    new MiniCssExtractPlugin({filename: 'build.css'}),
-  ],
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' }), new MiniCssExtractPlugin({ filename: 'build.css' })],
   module: {
-    rules:
-    [
+    rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.js$/,
@@ -32,13 +27,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env', 
-              '@babel/preset-react'
-            ]
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 };
